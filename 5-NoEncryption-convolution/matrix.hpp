b5ecafd284e3b8bd9ac413f15c16f248a999ff84
soba34 <165457207+soba34@users.noreply.github.com>
2025-03-12 23:15:51 +0900

    Create matrix.hpp

---
+++ 5-NoEncryption-convolution/matrix.hpp
@@ -0,0 +1,18 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <vector>
+
+// 行列型の定義
+using Matrix = std::vector<std::vector<double>>;
+
+// 行列の生成
+std::vector<std::vector<double>> generate_matrix(int rows, int cols, int external_seed);
+
+// 行列積の計算
+std::vector<std::vector<double>> multiply_matrices(
+    const std::vector<std::vector<double>>& A, 
+    const std::vector<std::vector<double>>& B
+);
+
+#endif // MATRIX_HPP
